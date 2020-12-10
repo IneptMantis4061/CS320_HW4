82a213c9e2f0ba5efb8296b6769028536c900a94
IneptMantis4061 <john191410@gmail.com>
2020-12-09 22:27:57 -0800

    Finished TestForPerformance test suite

--- testCorrectness.test.js
+++ testCorrectness.test.js
@@ -35,4 +35,18 @@ describe('TestForCorrectness', function() {
   });
 });
 
+describe('TestForPerformance', function() {
+  describe('MaxPrimeSum', function() {
+    it('maxPrimeSum(10000) should take less than ___ seconds', function() {
+      this.slow(0);
+      chai.expect(maxPrimeSum(10000)).to.be.an('array').of.length(2).to.deep.equal([9521,65]);
+    });
+
+    it('maxPrimeSum(100000) should take less than ___ seconds', function() {
+      this.slow(0);
+      chai.expect(maxPrimeSum(100000)).to.be.an('array').of.length(2).to.deep.equal([92951,183]);
+    });
+  });
+});
+
 
