a15bf1f969d24e206ba41cdb900f538ad491cdcc
IneptMantis4061 <john191410@gmail.com>
2020-12-09 22:11:40 -0800

    Finished MaxPrimeSum test suite

--- testCorrectness.test.js
+++ testCorrectness.test.js
@@ -3,35 +3,36 @@
 describe('TestForCorrectness', function() {
 
   describe('PrimeGen', function() {
-      describe('Test1', function () {
-        it('should return [2,3,5,7]', function() {
+    it('primeGen(10)=>[2, 3, 5, 7]', function() {
           chai.expect(primeGen(10)).to.be.an('array').of.length(4).to.deep.equal([2, 3, 5, 7]);
-        });
-      });
+    });
 
-      describe('Test2', function() {
-        it('should return [2, 3, 5, 7, 11, 13, 17, 19]', function() {
+    it('primeGen(20)=>[2, 3, 5, 7, 11, 13, 17, 19]', function() {
           chai.expect(primeGen(20)).to.be.an('array').of.length(8).to.deep.equal([2, 3, 5, 7, 11, 13, 17, 19]);
-        })
-      })
+        });
     });
 
   describe('CumulativeSum', function() {
-    describe('Test1', function() {
       const testNums = [1,2,3,4];
-      it('should return [1, 3, 6, 10]', function() {
+      it('cumulativeSum([1, 2, 3, 4])=>[1, 3, 6, 10]', function() {
         chai.expect(cumulativeSum(testNums)).to.be.an('array').of.length(4).to.deep.equal([1,3,6,10]);
       });
-    });
 
-    describe('Test2', function() {
-      const testNums = [10,11,12,13,14];
-      it('should return [10, 21, 33, 46, 60]', function() {
-        chai.expect(cumulativeSum(testNums)).to.be.an('array').of.length(5).to.deep.equal([10, 21, 33, 46, 60]);
+     const testNums1 = [10,11,12,13,14];
+      it('cumulativeSum([10, 11, 12, 13, 14])=>[10, 21, 33, 46, 60]', function() {
+        chai.expect(cumulativeSum(testNums1)).to.be.an('array').of.length(5).to.deep.equal([10, 21, 33, 46, 60]);
       });
-    });
   });
 
+  describe('MaxPrimeSum', function() {
+      it('maxPrimeSum(100)=>[41, 6]', function() {
+        chai.expect(maxPrimeSum(100)).to.be.an('array').of.length(2).to.deep.equal([41,6]);
+      });
+
+      it('maxPrimeSum(1000)=>[953, 21]', function() {
+        chai.expect(maxPrimeSum(1000)).to.be.an('array').of.length(2).to.deep.equal([953,21]);
+      });
+  });
 });
 
 
